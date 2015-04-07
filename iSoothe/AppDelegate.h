cade19e993372ac07d1d171926dcdaea11d8b8b7
Costas Vrahimis <cvrahimis.11@gmail.com>
2015-04-07 16:15:45 -0400

    Initial Commit

---
+++ iSoothe/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  iSoothe
+//
+//  Created by Costas Simiharv on 4/7/15.
+//  Copyright (c) 2015 cvrahimis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ iSoothe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iSoothe
+//
+//  Created by Costas Simiharv on 4/7/15.
+//  Copyright (c) 2015 cvrahimis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
